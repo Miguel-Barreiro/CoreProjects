d885be1de254a04b3b81ca63a44971bc1dfb2c75
MiguelBarreiro <miguel.barreiro.mail@gmail.com>
2025-03-06 02:21:55 +0000

    made IPositionEntity a bigger parent for the physics entities

--- Runtime/Core/Model/IPhysicsEntity.cs
+++ Runtime/Core/Model/IPhysicsEntity.cs
@@ -7,6 +7,8 @@ namespace Core.Model
 {
     public interface I2DPhysicsEntity : IPositionEntity
     {
+        Vector2 Position { get; set; }
+        GameObject Prefab { get; set; }
         Rigidbody2D Rigidbody2D { get; set; }
     }
     
