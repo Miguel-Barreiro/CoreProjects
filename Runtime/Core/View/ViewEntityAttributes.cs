6877b91e10d9964258c29dec90d9e802ff328f5b
MiguelBarreiro <miguel.barreiro.mail@gmail.com>
2025-03-28 15:12:00 +0000

    added caching for views when they dont exist

--- Runtime/Core/View/ViewEntityAttributes.cs
+++ Runtime/Core/View/ViewEntityAttributes.cs
@@ -18,7 +18,7 @@ namespace Core.View
 		public EntId ID { get; }
 		public GameObject? GameObject { get; internal set; }
 
-		private readonly Dictionary<Type, object> ComponentCache = new ();
+		private readonly Dictionary<Type, object?> ComponentCache = new ();
 		
 		// public T? Get<T>() 
 		// 	where T : Component
@@ -51,16 +51,17 @@ namespace Core.View
 			
 			Type componentType = typeof(T);
 			if (ComponentCache.TryGetValue(componentType, out object? value))
-				return (T)value;
+				return (T?)value;
 			
-			T component = GameObject.GetComponent<T>();
+			T? component = GameObject.GetComponent<T>();
 			if (component == null)
 			{
 				component = GameObject.GetComponentInChildren<T>();
 				if (component == null)
 				{
-					Debug.LogError($"did not find {componentType.Name} component in {GameObject.name}");
-					return default(T);
+					// Debug.LogError($"did not find {componentType.Name} component in {GameObject.name}");
+					
+					component = default(T);
 				}
 			}
 			ComponentCache.Add(componentType, component);
